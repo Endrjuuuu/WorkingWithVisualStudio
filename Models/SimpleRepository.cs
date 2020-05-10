8d3fc8e750aa1afadf4813980b845c232083e9a9
Andrzej Witkowski <endrjus8@gmail.com>
2020-05-10 14:23:38 +0200

    Add project files.

---
+++ Models/SimpleRepository.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections.Generic;
+
+namespace WorkingWithVisualStudio.Models
+{
+    public class SimpleRepository
+    {
+        private static SimpleRepository sharedRepository = new SimpleRepository();
+        private Dictionary<string, Product> products = new Dictionary<string, Product>();
+        public static SimpleRepository SharedRepository => sharedRepository;
+
+        public SimpleRepository()
+        {
+            var initialItems = new[]
+            {
+                new Product {Name = "Kajak", Price = 275M},
+                new Product {Name = "Kamizelka ratunkowa", Price = 48.95M},
+                new Product {Name = "Piłka nożna", Price = 19.50M},
+                new Product {Name = "Flaga narożna", Price = 34.95M}
+            };
+            foreach (var p in initialItems)
+            {
+                AddProduct(p);
+            }
+        }
+
+        public IEnumerable<Product> Products => products.Values;
+
+        public void AddProduct(Product p) => products.Add(p.Name, p);
+    }
+}
